33c84b8fb1d08d7a7f10fcd8d48561af9bad50c9
Justin Hink <jhink@mednet.ucla.edu>
2026-08-24 08:05:38 -0700

    renamed RTHistory

--- README.md
+++
@@ -1,2 +0,0 @@
-# RTHistory
-This code collects all the RT dicoms that connect to a treatment plan
--- src/__main__.py
+++
@@ -1,5 +0,0 @@
-# Import your actual modules
-from .main import start
-
-if __name__ == "__main__":
-    start()
